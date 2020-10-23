931a596f71da17601f120e1acf9c0219413689e4
jardianoalmeida <jardiano.almeida@gmail.com>
2020-10-23 19:21:40 -0400

    Managed beans

---
+++ src/main/java/br/com/jardiano/OlaMundoBean.java
@@ -0,0 +1,22 @@
+package br.com.jardiano;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class OlaMundoBean {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void enviar() {
+		this.setNome(this.getNome().toUpperCase());
+	}
+	
+}
