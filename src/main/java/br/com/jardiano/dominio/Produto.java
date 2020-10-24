224663d616493370967343c3d86a6ffd7654611c
jardianoalmeida <jardiano.almeida@gmail.com>
2020-10-24 11:24:16 -0400

    Manipulando eventos de mudança de valor

---
+++ src/main/java/br/com/jardiano/dominio/Produto.java
@@ -0,0 +1,30 @@
+package br.com.jardiano.dominio;
+
+import java.io.Serializable;
+
+public class Produto implements Serializable {
+
+	private String nome;
+	private String fabricante;
+	private String categoria;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+}
---
+++ src/main/java/br/com/jardiano/visao/GestaoProdutosBean.java
@@ -0,0 +1,82 @@
+package br.com.jardiano.visao;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ValueChangeEvent;
+
+import br.com.jardiano.dominio.Produto;
+
+
+@ManagedBean
+@SessionScoped
+public class GestaoProdutosBean implements Serializable {
+
+	private String nomePesquisa;
+	private List<Produto> produtos;
+	private List<Produto> produtosFiltrados;
+	private Produto produto;
+	
+	private Produto produtoSelecionado;
+
+	public GestaoProdutosBean() {
+		this.produtos = new ArrayList<Produto>();
+		this.produtosFiltrados = new ArrayList<Produto>();
+		this.produto = new Produto();
+	}
+	
+	public void nomePesquisaAlterado(ValueChangeEvent event) {
+		System.out.println("Valor atual: " + this.nomePesquisa);
+		System.out.println("Novo valor: " + event.getNewValue());
+		
+		this.produtosFiltrados.clear();
+		
+		for (Produto produto : this.produtos) {
+			if (produto.getNome() != null && produto.getNome().toLowerCase()
+					.startsWith(event.getNewValue().toString().toLowerCase())) {
+				this.produtosFiltrados.add(produto);
+			}
+		}
+	}
+	
+	public void incluir() {
+		this.produtos.add(this.produto);
+		this.produto = new Produto();
+	}
+	
+	public void excluir() {
+		this.produtos.remove(this.produtoSelecionado);
+	}
+	
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public Produto getProdutoSelecionado() {
+		return produtoSelecionado;
+	}
+
+	public void setProdutoSelecionado(Produto produtoSelecionado) {
+		this.produtoSelecionado = produtoSelecionado;
+	}
+
+	public String getNomePesquisa() {
+		return nomePesquisa;
+	}
+
+	public void setNomePesquisa(String nomePesquisa) {
+		this.nomePesquisa = nomePesquisa;
+	}
+
+	public List<Produto> getProdutosFiltrados() {
+		return produtosFiltrados;
+	}
+	
+}
