fb260a3bc24552caf1852b4e8cd2dca686f05a28
jardianoalmeida <jardiano.almeida@gmail.com>
2020-10-23 22:16:52 -0400

    Exercício: managed beans e seus escopos

---
+++ src/main/java/br/com/jardiano/dominio/Livro.java
@@ -0,0 +1,33 @@
+package br.com.jardiano.dominio;
+
+import java.io.Serializable;
+
+public class Livro implements Serializable {
+
+	private String titulo;
+	private String autores;
+	private String assunto;
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutores() {
+		return autores;
+	}
+	public void setAutores(String autores) {
+		this.autores = autores;
+	}
+	public String getAssunto() {
+		return assunto;
+	}
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+	
+	
+	
+}
--- src/main/java/br/com/jardiano/visao/CatalogoLivrosBean.java
+++ src/main/java/br/com/jardiano/visao/CatalogoLivrosBean.java
@@ -4,41 +4,45 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.NoneScoped;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
-
+import javax.faces.bean.ViewScoped;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-import br.com.jardiano.dominio.Produto;
+import br.com.jardiano.dominio.Livro;
 
 @ManagedBean
-@NoneScoped // Não guarda nada
+//none, request, view, session e application
+// @NoneScoped // Não guarda nada
 // @RequestScoped // Nada fica guardado na tela
 // @SessionScoped // Mant�m os dados salvos visiveis para todos os usuarios
-public class GestaoProdutosBean implements Serializable{
+//@ViewScoped
+@ApplicationScoped
+public class CatalogoLivrosBean implements Serializable{
 
-	private List<Produto> produtos;
-	private Produto produto;
+	private List<Livro> livros;
+	private Livro livro;
 	
-	public GestaoProdutosBean() {
-		this.produtos = new ArrayList<Produto>();
-		this.produto = new Produto();
+	public CatalogoLivrosBean() {
+		this.livros = new ArrayList<Livro>();
+		this.livro = new Livro();
 	}
 	
 	public void incluir() {
-		this.produtos.add(this.produto);
-		this.produto = new Produto();
+		this.livros.add(this.livro);
+		this.livro = new Livro();
 	}
 
-	public Produto getProduto() {
-		return produto;
+	public Livro getLivro() {
+		return livro;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public List<Livro> getLivros() {
+		return livros;
 	}
 	
 	@PostConstruct
