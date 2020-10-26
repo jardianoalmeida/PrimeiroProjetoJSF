81f0a0647bba2b2e92fecfc3a2b3c221215bb38c
jardianoalmeida <jardiano.almeida@gmail.com>
2020-10-26 10:18:54 -0400

    Exclusão de lançamentos do banco de dados

--- src/main/java/br/com/jardiano/view/ConsultaLancamentoBean.java
+++ src/main/java/br/com/jardiano/view/ConsultaLancamentoBean.java
@@ -5,23 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-
-import br.com.jardiano.model.Lancamento;
-import br.com.jardiano.util.HibernateUtil;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Order;
+
+import br.com.jardiano.model.Lancamento;
+import br.com.jardiano.util.FacesUtil;
+import br.com.jardiano.util.HibernateUtil;
+
 @ManagedBean
 public class ConsultaLancamentoBean implements Serializable {
 
 	private List<String> lancamentos = new ArrayList<String>();
+	private Lancamento lancamentoSelecionado;
 
 	@SuppressWarnings("unchecked")
 	@PostConstruct
@@ -34,8 +34,34 @@ public class ConsultaLancamentoBean implements Serializable {
 		session.close();
 	}
 
+	public void excluir() {
+		if (this.lancamentoSelecionado.isPago()) {
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, "Lançamento já foi pago e não pode ser excluído.");
+		} else {
+			Session session = HibernateUtil.getSession();
+			Transaction trx = session.beginTransaction();
+			
+			session.delete(this.lancamentoSelecionado);
+			
+			trx.commit();
+			session.close();
+			
+			this.inicializar();
+			
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_INFO, "Lançamento excluído com sucesso!");
+		}
+	}
+	
 	public List<String> getLancamentos() {
 		return lancamentos;
+	}
+	
+	public Lancamento getLancamentoSelecionado() {
+		return lancamentoSelecionado;
+	}
+
+	public void setLancamentoSelecionado(Lancamento lancamentoSelecionado) {
+		this.lancamentoSelecionado = lancamentoSelecionado;
 	}
 
 }
